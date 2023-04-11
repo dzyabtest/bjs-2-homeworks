4a45b8e3f8ead570b010efc53881b906fd1ccacc
dzyabtest <dzyab@mail.ru>
2023-04-11 20:54:55 +0300

    ДЗ 1.1, 1.2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
+    let arr = [];
+    let D;
+    // код для задачи №1 писать здесь
   
-  return arr;
+    D = b**2 - 4*a*c;
+  
+    if (D === 0){
+      arr.push(-b/(2*a));
+    }
+    else if (D > 0){
+      arr.push((-b + Math.sqrt(D) )/(2*a));
+      arr.push((-b - Math.sqrt(D) )/(2*a));
+    }
+
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let totalAmount;
+  let monPercet;
+  let body;
+  let monPayment;
+
+  monPercet = percent/100/12;
+  body = amount - contribution;
+
+  monPayment = body * (monPercet + (monPercet / (((1 + monPercet)**countMonths)-1)));
+  totalAmount = monPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
   
 }
