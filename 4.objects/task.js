2242ef47f265bd07f64f39f18f17fcaac4f1770e
dzyabtest <dzyab@mail.ru>
2023-04-13 23:26:47 +0300

    ДЗ 4.1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,50 @@
 function Student(name, gender, age) {
-  
+    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  //ваш код
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+// ваш код для остальных методов
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.excluded != undefined) {
+    return 0;
+  }
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  this.marks.push(...marksToAdd);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if ((this.marks === undefined)||(this.marks.length === 0)) {
+    return 0;
+  }
+  else {
+    return this.marks.reduce((acc, item) => acc + item, 0)/this.marks.length;
+  }
+
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+  if (this.marks === undefined) {
+    return 0;
+  }
+
+  if (this.excluded != undefined) {
+    return 0;
+  }
+
+  else {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+    return 1;
+  }
 }
