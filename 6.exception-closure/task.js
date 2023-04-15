464c109994cf418988f88bbfac1caebea66247a0
dzyabtest <dzyab@mail.ru>
2023-04-15 23:16:50 +0300

    ДЗ 6.1, 6.2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+﻿// Задача 1
+
+function parseCount(inputValue){
+    let number;
+
+    number = Number.parseFloat(inputValue);
+
+    if (isNaN(number)){
+        throw new Error('Невалидное значение');
+    }
+    else {
+        return number;
+    }
+}
+
+function validateCount(inputValue){
+    try {
+        return parseCount(inputValue);
+    }
+    catch (error) {
+        return error;
+    }
+}
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c){
+        if ((a >= (b + c)) || (b >= (a + c)) || (c >= (a + b))) {
+            throw new Error('Треугольник с такими сторонами не существует');
+            return;
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area(){
+        let semiPerimetr, area;
+
+        semiPerimetr = this.perimeter/2;
+        area = Math.sqrt(semiPerimetr * (semiPerimetr - this.a) * (semiPerimetr - this.b) * (semiPerimetr - this.c));
+
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c){
+    const errMessage = 'Ошибка! Треугольник не существует';
+    try {
+        return new Triangle(a, b, c);
+    }
+    catch {
+        return {get perimeter() {return errMessage}, get area(){return errMessage}};
+    }
+}
